247d05ffd561360387f88fc7119ecff86ca67ade
phillipsK <kjphillips@suffolk.edu>
2016-10-05 10:42:18 -0400

    added listview and arrays

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.android.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static android.R.attr.data;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -57,6 +63,31 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            /* StackOverflow Version
+            http://stackoverflow.com/questions/1005073/initialization-of-an-arraylist-in-one-line */
+            ArrayList<String> Weahter_list = new ArrayList<>();
+
+            Weahter_list.add("Today - Sunny - 88/63");
+            Weahter_list.add("Tomorrow - Foggy - 70/46");
+            Weahter_list.add("Weds - Rain - 65/56");
+            Weahter_list.add("Thurs - Cloudy - 65/56");
+            Weahter_list.add("Fri - Cloudy - 65/56");
+            Weahter_list.add("Sat - Sunny - 65/56");
+
+            /* Udacity version */
+            String[] AltWeather_list = {
+                    "Mon 6/23 - Sunny - 31/17",
+                    "Tue 6/24 - Foggy - 21/8",
+                    "Wed 6/25 - Cloudy - 22/17",
+                    "Thurs 6/26 - Rainy - 18/11",
+                    "Fri 6/27 - Foggy - 21/10",
+                    "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                    "Sun 6/29 - Sunny - 20/7"
+            };
+
+            List<String> weekForecast = new ArrayList<String>(Arrays.asList(AltWeather_list));
+
             return rootView;
         }
     }
