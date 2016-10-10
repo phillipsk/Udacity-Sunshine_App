06f88baea0f343e8b6d148766530a634b096aaeb
phillipsK <kjphillips@suffolk.edu>
2016-10-09 21:50:39 -0400

    refactored Fragment class

---
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -0,0 +1,180 @@
+package com.example.android.sunshine.app;
+
+/**
+ * Created by tech_ministry on 10/9/16.
+ */
+
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ForecastFragment extends Fragment {
+
+    /* Udacity initializes the adapter outside */
+//    ArrayAdapter<String> mForecastAdapter;
+//    changed to private after refactoring ForecastFragment class from MainActivity.java
+    ArrayAdapter<String> mForecastAdapter;
+
+
+    public ForecastFragment(){
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            /* StackOverflow Version
+            http://stackoverflow.com/questions/1005073/initialization-of-an-arraylist-in-one-line */
+        ArrayList<String> Weahter_list = new ArrayList<>();
+
+        Weahter_list.add("Today - Sunny - 88/63");
+        Weahter_list.add("Tomorrow - Foggy - 70/46");
+        Weahter_list.add("Weds - Rain - 65/56");
+        Weahter_list.add("Thurs - Cloudy - 65/56");
+        Weahter_list.add("Fri - Cloudy - 65/56");
+        Weahter_list.add("Sat - Sunny - 65/56");
+
+            /* Udacity version */
+        String[] AltWeather_list = {
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        };
+
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(AltWeather_list));
+
+            /* Udacity Version of constructing the adapter */
+        mForecastAdapter =
+                new ArrayAdapter<String>(
+                        getActivity(), // The current context (this activity)
+                        R.layout.list_item_forecast, // The name of the layout ID.
+                        R.id.list_item_forecast_textview, // The ID of the textview to populate.
+                        weekForecast); // The array called
+
+
+//            My attempt at initializing and constructing an adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,weekForecast);
+
+//            Call the adapter according to the documentation not yet learned on Udacity
+
+/*            ListView listView = (R.layout.list_item_forecast) findViewById(R.layout.list_item_forecast);
+            listView.setAdapter(adapter);*/
+
+//            FrameLayout listview_forecast = this.find
+            /* Udacity version of 1) finding the list view and 2) calling the view with the adapter method*/
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(adapter);
+
+
+        return rootView;
+    }
+
+
+    public class FetchWeatherTask extends AsyncTask<Void,Void,Void>{
+
+        //            Prints the name of this class in the logs, for easier debugging
+        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+//            ****************************************************************************
+//            API code from https://gist.github.com/anonymous/1c04bf2423579e9d2dcd
+//            ****************************************************************************
+
+            // These two need to be declared outside the try/catch
+            //  so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are available at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                /* sample URL url initialization */
+//                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+//                Even though the API docs specify JSON as the default response syntax, explicitly
+//                defining it anyways isolates any errors
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=02050&mode=json&units=metric&cnt=7&appid=b70d61271792d0fbb5c045701c8bf245");
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    forecastJsonStr = null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    forecastJsonStr = null;
+                }
+                forecastJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e("ForecastFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attempting
+                // to parse it.
+                forecastJsonStr = null;
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("ForecastFragment", "Error closing stream", e);
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
+
--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,19 +1,9 @@
 package com.example.android.sunshine.app;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -24,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -51,70 +41,5 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
 
-        /* Udacity initializes the adapter outside */
-        ArrayAdapter<String> mForecastAdapter;
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            /* StackOverflow Version
-            http://stackoverflow.com/questions/1005073/initialization-of-an-arraylist-in-one-line */
-            ArrayList<String> Weahter_list = new ArrayList<>();
-
-            Weahter_list.add("Today - Sunny - 88/63");
-            Weahter_list.add("Tomorrow - Foggy - 70/46");
-            Weahter_list.add("Weds - Rain - 65/56");
-            Weahter_list.add("Thurs - Cloudy - 65/56");
-            Weahter_list.add("Fri - Cloudy - 65/56");
-            Weahter_list.add("Sat - Sunny - 65/56");
-
-            /* Udacity version */
-            String[] AltWeather_list = {
-                    "Mon 6/23 - Sunny - 31/17",
-                    "Tue 6/24 - Foggy - 21/8",
-                    "Wed 6/25 - Cloudy - 22/17",
-                    "Thurs 6/26 - Rainy - 18/11",
-                    "Fri 6/27 - Foggy - 21/10",
-                    "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-                    "Sun 6/29 - Sunny - 20/7"
-            };
-
-            List<String> weekForecast = new ArrayList<String>(Arrays.asList(AltWeather_list));
-
-            /* Udacity Version of constructing the adapter */
-            mForecastAdapter =
-                new ArrayAdapter<String>(
-                         getActivity(), // The current context (this activity)
-                         R.layout.list_item_forecast, // The name of the layout ID.
-                         R.id.list_item_forecast_textview, // The ID of the textview to populate.
-                         weekForecast); // The array called
-
-
-//            My attempt at initializing and constructing an adapter
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,
-                    R.id.list_item_forecast_textview,weekForecast);
-
-//            Call the adapter according to the documentation not yet learned on Udacity
-
-/*            ListView listView = (R.layout.list_item_forecast) findViewById(R.layout.list_item_forecast);
-            listView.setAdapter(adapter);*/
-
-//            FrameLayout listview_forecast = this.find
-            /* Udacity version of 1) finding the list view and 2) calling the view with the adapter method*/
-            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-            listView.setAdapter(adapter);
-
-            return rootView;
-        }
-    }
 }
