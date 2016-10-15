4fc10009aa34defa6ca1f3f3f07a07121ae77220
phillipsK <kjphillips@suffolk.edu>
2016-10-15 09:54:09 -0400

    moved SettingsActivity to the correct package; addPreferencesFromResource && addPreferencesFromResource

--- app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
@@ -1,12 +1,10 @@
 package com.example.android.sunshine.app;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
-import android.view.KeyEvent;
 
 /**
  * A {@link PreferenceActivity} that presents a set of application settings.
@@ -23,15 +21,16 @@ public class SettingsActivity extends PreferenceActivity
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Add 'general' preferences, defined in the XML file
-        // TODO: Add preferences from XML
+
+        addPreferencesFromResource(R.xml.pref_general);
 
 //        deprecated from Android documentation
 //        addPreferencesFromResource(R.xml.preferences);
 
-
         // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
         // updated when the preference changes.
-        // TODO: Add preferences
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
+
     }
 
     /**
