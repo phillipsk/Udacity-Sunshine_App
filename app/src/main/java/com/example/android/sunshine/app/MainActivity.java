c4a8cf2a69160130df62049a883aa8465a242bf9
phillipsK <kjphillips@suffolk.edu>
2016-10-21 22:00:10 -0400

    adding map intent

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.sunshine.app;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
@@ -58,7 +59,19 @@ public class MainActivity extends ActionBarActivity {
 
     private void openPreferredLocationInMap(){
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        String location =
+        String location = sharedPrefs.getString(getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default));
+
+        // Using the URI scheme for showing a location found on a map.  This super-handy
+        // intent can is detailed in the "Common Intents" page of Android's developer site:
+        // http://developer.android.com/guide/components/intents-common.html#Maps
+
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q",location)
+                .build();
+
+
+
     }
 
 
